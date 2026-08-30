a59648e76cddc63e544dc95874d937ed35c4950d
Vermach Sontyo <vermachendless@gmail.com>
2026-08-30 07:10:52 +0100

    deployed sanity studio live url

--- studio-kuunloans-backend/sanity.cli.ts
+++ studio-kuunloans-backend/sanity.cli.ts
@@ -5,7 +5,9 @@ export default defineCliConfig({
     projectId: 'd1cwze7g',
     dataset: 'production'
   },
+  studioHost: 'kuunloans',
   deployment: {
+    appId: 'n8qk0lj1u16wpf15gj6zj39k',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
@@ -13,3 +15,5 @@ export default defineCliConfig({
     autoUpdates: true,
   },
 })
+
+
