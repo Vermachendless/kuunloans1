16801a3f27a391ee0cb50d6eddb86155a6a1354f
Vermach Sontyo <vermachendless@gmail.com>
2026-08-31 16:37:47 +0100

    feat: add KuunLoansLogo component and update tsconfig include/exclude paths

--- studio-kuunloans-backend/components/KuunLoansLogo.tsx
+++ studio-kuunloans-backend/components/KuunLoansLogo.tsx
@@ -1,17 +1,34 @@
 import React from 'react'
 
-export function KuunLoansLogo() {
+export function KuunLoansLogo(props: React.HTMLAttributes<HTMLSpanElement>) {
   return (
-    <img
-      src="/static/logo.png"
-      alt="KuunLoans Logo"
+    <span
       style={{
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover',
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '1.2em',
+        height: '1.2em',
         borderRadius: '50%',
-        display: 'block',
+        overflow: 'hidden',
+        flexShrink: 0,
+        backgroundColor: '#F4C430',
+        border: '1px solid rgba(244, 196, 48, 0.4)',
+        boxSizing: 'border-box',
+        ...props.style,
       }}
-    />
+      {...props}
+    >
+      <img
+        src="/static/logo.png"
+        alt="KuunLoans Logo"
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          display: 'block',
+        }}
+      />
+    </span>
   )
 }
